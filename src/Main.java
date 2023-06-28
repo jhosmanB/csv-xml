7dfb4fb6820eade286602ea1295f57beeca08a21
jhosmanB <jhosmanbabery@gmail.com>
2023-06-28 13:01:14 -0400

    creacion de xml segun modelos

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,56 @@
 import java.util.List;
+import java.util.Scanner;
 
 import backend.archivo.ControladorArchivo;
+import backend.xml.XMLModification;
 
 public class Main {
     
     public static void main(String[] args) throws Exception {
-         final String PATH_CSV  = "phone.csv";
+    Scanner sc = new Scanner(System.in); 
+    System.out.println("Ingrese la direccion ip  de servidor");
+     String ip = sc.nextLine();
+     System.out.println("Ingrese la contrasena de las extensiones");
+     String contra = sc.nextLine();
+        final String PATH_CSV  = "Phone_htms Resumen(delimitado con comas).csv";
         ControladorArchivo archivo = new ControladorArchivo(PATH_CSV);
         List<String> contenido = archivo.obtenerArchivo();
+        System.out.println(contenido.size());
         for (int i = 0; i < contenido.size(); i++) {
             String res = contenido.get(i);
             String[] parts = res.split("\\,");
             
-    if (parts.length > 190) {
+  
        String nombreArchivo = parts[0];
-            String name = parts[190];
-            String tipo = parts[41];
-            String extension = parts[140];
+            String name = parts[7];
+            String modelo = parts[1];
+            String extension = parts[4];
             String modeloXml;
-            if (tipo.equalsIgnoreCase("Cisco 3905")) {
+            int  tipo;
+            if (modelo.equalsIgnoreCase("Cisco 3905")) {
                 modeloXml = "modelosXML\\CP-3905\\SEPF07816A2C103.cnf.xml_22205";
+                tipo = 1;
             }
             else{
-                if (tipo.equalsIgnoreCase("Cisco 7821")) {
+                if (modelo.equalsIgnoreCase("Cisco 7821")) {
                     modeloXml = "modelosXML\\CP-7861\\SEPF07816D0A358.cnf.xml_22200";
+                    tipo = 2;
                 }
                 else{
-                      if (tipo.equalsIgnoreCase("Cisco 7861")) {
+                      if (modelo.equalsIgnoreCase("Cisco 7861")) {
                         modeloXml="modelosXML\\CP-7861\\SEPF07816D0A358.cnf.xml_22200";
+                        tipo = 2;
                       }
                       else {
                         modeloXml ="modelosXML\\CP-8945\\SEP204C9E6CB941.cnf.xml_22202";
+                        tipo = 2;
                       }
                     }
             }
-        }
+           
+            XMLModification modification = new XMLModification(nombreArchivo,name,extension,modeloXml,tipo,ip,contra);
+            modification.construirArchivos();
+        
         
         }
       
--- src/backend/xml/XMLModification.java
+++ src/backend/xml/XMLModification.java
@@ -6,6 +6,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import javax.print.DocFlavor.STRING;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,37 +18,225 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 public class XMLModification {
+    private String nombreArchivo;
     private String name;
     private String extension;
     private String path;
-    public XMLModification(String name, String extension, String path) {
+    private int tipo;
+    private String ip;
+    private String contra;
+    public XMLModification(String nombreArchivo,String name, String extension, String path, int  tipo, String ip,String contra) {
+        this.nombreArchivo = nombreArchivo;
         this.extension = extension;
         this.name = name;
         this.path = path;
+        this.tipo = tipo;
+        this.ip = ip;
+        this.contra = contra;
     }
     public  void construirArchivos() throws IOException{
         String fileName = this.path;
-        String modifiedFileName = this.name + ".xml";
-
-        try {
+        String modifiedFileName = this.nombreArchivo + ".cnf.xml";
+        switch (this.tipo) {
+            case 1:
+                modificacion1(fileName, modifiedFileName);
+                break;
+            case 2:
+                modificacion2(fileName, modifiedFileName);
+                break;
+            default:
+                break;
+        }
+       
+    }
+    private void modificacion1  (String fileName, String modifiedFileName) throws IOException{
+         try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.parse(fileName);
 
-            // Modificar la etiqueta <featureLabel> en la línea 106
+            NodeList processNodeNameList = doc.getElementsByTagName("processNodeName");
+            if (processNodeNameList.getLength() > 0) {
+                Node processNodeNameNode = processNodeNameList.item(0);
+                if (processNodeNameNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element processNodeNameElement = (Element) processNodeNameNode;
+                    processNodeNameElement.setTextContent(this.ip);
+                }
+            }
+            
             NodeList lineList = doc.getElementsByTagName("line");
             if (lineList.getLength() > 0) {
                 Node lineNode = lineList.item(0);
                 if (lineNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element lineElement = (Element) lineNode;
-                    NodeList featureLabelList = lineElement.getElementsByTagName("featureLabel");
-                    if (featureLabelList.getLength() > 0) {
-                        Node featureLabelNode = featureLabelList.item(0);
+                    NodeList phoneLabelList = lineElement.getElementsByTagName("phoneLabel");
+                    if (phoneLabelList.getLength() > 0) {
+                        Node featureLabelNode = phoneLabelList.item(0);
                         if (featureLabelNode.getNodeType() == Node.ELEMENT_NODE) {
                             Element featureLabelElement = (Element) featureLabelNode;
-                            featureLabelElement.setTextContent("Prueba");
+                            featureLabelElement.setTextContent(this.extension);
                         }
                     }
+                    NodeList sipLinesList = doc.getElementsByTagName("sipLines");
+                    if (sipLinesList.getLength() > 0) {
+                        Node sipLinesNode = sipLinesList.item(0);
+                        if (sipLinesNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element sipLinesElement = (Element) sipLinesNode;
+                            NodeList fetuareLabelList = sipLinesElement.getElementsByTagName("featureLabel");
+                            if (fetuareLabelList.getLength() > 0) {
+                                Node nameNode = fetuareLabelList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList proxyList = sipLinesElement.getElementsByTagName("proxy");
+                            if (proxyList.getLength() > 0) {
+                                Node nameNode = proxyList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.ip);
+                                }
+                            }
+                            NodeList nameList = sipLinesElement.getElementsByTagName("name");
+                            if (nameList.getLength() > 0) {
+                                Node nameNode = nameList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList displayNameList = sipLinesElement.getElementsByTagName("displayName");
+                            if (displayNameList.getLength() > 0) {
+                                Node nameNode = displayNameList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList authNameList = sipLinesElement.getElementsByTagName("authName");
+                            if (authNameList.getLength() > 0) {
+                                Node nameNode = authNameList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList contactList = sipLinesElement.getElementsByTagName("contact");
+                            if (contactList.getLength() > 0) {
+                                Node nameNode = contactList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList authPasswordList = sipLinesElement.getElementsByTagName("authPassword");
+                            if (authPasswordList.getLength() > 0) {
+                                Node nameNode = authPasswordList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.contra);
+                                }
+                            }
+                        }
+                    } 
+                }
+            }
+
+            // Escribir el archivo XML modificado
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(modifiedFileName));
+            transformer.transform(source, result);
+
+            System.out.println("Archivo XML modificado creado: " + modifiedFileName);
+        } catch (ParserConfigurationException | IOException | org.xml.sax.SAXException | TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+    private void modificacion2(String fileName, String modifiedFileName){
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(fileName);
+
+            NodeList processNodeNameList = doc.getElementsByTagName("processNodeName");
+            if (processNodeNameList.getLength() > 0) {
+                Node processNodeNameNode = processNodeNameList.item(0);
+                if (processNodeNameNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element processNodeNameElement = (Element) processNodeNameNode;
+                    processNodeNameElement.setTextContent(this.ip);
+                }
+            }
+            NodeList lineList = doc.getElementsByTagName("line");
+            if (lineList.getLength() > 0) {
+                Node lineNode = lineList.item(0);
+                if (lineNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element lineElement = (Element) lineNode;
+                    NodeList phoneLabelList = lineElement.getElementsByTagName("phoneLabel");
+                    if (phoneLabelList.getLength() > 0) {
+                        Node featureLabelNode = phoneLabelList.item(0);
+                        if (featureLabelNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element featureLabelElement = (Element) featureLabelNode;
+                            featureLabelElement.setTextContent(this.extension);
+                        }
+                    }
+                    NodeList sipLinesList = doc.getElementsByTagName("sipLines");
+                    if (sipLinesList.getLength() > 0) {
+                        Node sipLinesNode = sipLinesList.item(0);
+                        if (sipLinesNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element sipLinesElement = (Element) sipLinesNode;
+                            NodeList fetuareLabelList = sipLinesElement.getElementsByTagName("featureLabel");
+                            if (fetuareLabelList.getLength() > 0) {
+                                Node nameNode = fetuareLabelList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList nameList = sipLinesElement.getElementsByTagName("name");
+                            if (nameList.getLength() > 0) {
+                                Node nameNode = nameList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList displayNameList = sipLinesElement.getElementsByTagName("displayName");
+                            if (displayNameList.getLength() > 0) {
+                                Node nameNode = displayNameList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList authNameList = sipLinesElement.getElementsByTagName("authName");
+                            if (authNameList.getLength() > 0) {
+                                Node nameNode = authNameList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList contactList = sipLinesElement.getElementsByTagName("contact");
+                            if (contactList.getLength() > 0) {
+                                Node nameNode = contactList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.extension);
+                                }
+                            }
+                            NodeList authPasswordList = sipLinesElement.getElementsByTagName("authPassword");
+                            if (authPasswordList.getLength() > 0) {
+                                Node nameNode = authPasswordList.item(0);
+                                if (nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element nameElement = (Element) nameNode;
+                                    nameElement.setTextContent(this.contra);
+                                }
+                            }
+                        }
+                    } 
                 }
             }
 
@@ -63,3 +253,8 @@ public class XMLModification {
         }
     }
 }
+
+         
+
+
+            
